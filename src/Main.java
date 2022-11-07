d7d21873e30ec64bd02f52ed3833775712405c11
Erasto-M <erastusmunya2022@gmail.com>
2022-11-07 10:53:45 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        hello x = new hello();
+        x.name = "munyao";
+        x.year = 2022;
+        x.muras = "munyao";
+        x.dekut();
+        x.murasta();
+    }
+}
---
+++ src/hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    String name;
+    int year;
+    String muras;
+    void dekut(){
+       System.out.println(name + "\t welcome");
+        System.out.println(year + "\t was my best year");
+    }
+    void murasta(){
+        System.out.println(muras +"Hello world!");
+    }
+
+}
